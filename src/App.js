3aec05cbe7441ae24c48e7c5df75cfd0108a16ff
Sean Kramer <seandominickramer@gmail.com>
2021-01-07 21:12:56 -0500

    Refactor Products component to use Context

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import { ProductContext } from './Contexts/ProductContext'
 
 // Components
 import Navigation from './components/Navigation';
@@ -18,16 +19,18 @@ function App() {
 
 	return (
 		<div className="App">
+			<ProductContext.Provider value={{products, addItem}}>
 			<Navigation cart={cart} />
 
 			{/* Routes */}
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products />
 			</Route>
 
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
 			</Route>
+			</ProductContext.Provider>
 		</div>
 	);
 }
